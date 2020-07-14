686e447d5d4abe876a6c8ce1731ad164fdd938d9
Andrew Williams <andy@tensixtyone.com>
2020-07-14 16:21:04 +0100

    Add Powershell Profile

---
+++ powershell/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+# Add a SSH key to SSH-Agent
+function Add-Key {
+    if ($args.Count -gt 0) {
+        $filename = "$ENV:USERPROFILE/.ssh/id_ed25519_" + $args[0]
+        if (!(Test-Path $filename -PathType Leaf)) {
+            Write-Error "$filename does not exist."
+        }
+        else {
+            Invoke-Expression "ssh-add $filename"   
+        }
+    }
+    else {
+        Invoke-Expression "ssh-add -L"
+    }
+}
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
