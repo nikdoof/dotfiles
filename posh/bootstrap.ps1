d0588730f2972e36edbe142fe7788a1dc68c00a0
Andrew Williams <andy@tensixtyone.com>
2020-07-14 20:53:14 +0100

    Correct GPG bootstrap

--- posh/bootstrap.ps1
+++ posh/bootstrap.ps1
@@ -7,4 +7,4 @@ Install-Module -Name PSDotFiles -Scope CurrentUser
 # Fix git, SSH and GPG to external executables
 [Environment]::SetEnvironmentVariable('GIT_SSH', "$ENV:SystemRoot\System32\OpenSSH\ssh.exe", 'User')
 [Environment]::SetEnvironmentVariable('GNUPGHOME', "$ENV:APPDATA\gnupg", 'User')
-Invoke-Expression "git config --global gpg.program '/c/Program Files (x86)/GnuPG/bin/gpg.exe'"
+Invoke-Expression "git config --global gpg.program 'c:/Program Files (x86)/GnuPG/bin/gpg.exe'"
