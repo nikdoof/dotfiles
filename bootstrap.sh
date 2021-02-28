fa4772616d41a0583df18166986448acbbb744b7
Andrew Williams <andy@tensixtyone.com>
2021-02-28 10:51:44 +0000

    Echo out what is going on

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone git@github.com:nikdoof/dotfiles.git $HOME/.dotfiles
 cd $HOME/.dotfiles/
-for package in "bin bash"; do
+for package in bin bash; do
+  echo "Stowing ${package}"
   ./bin/bin/stowage $package
 done
