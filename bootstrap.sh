93e7b4d8b06386bb33a456c4425b2b8e79688852
Andrew Williams <andy@tensixtyone.com>
2021-03-05 10:57:56 +0000

    Update bootstrap to remove bash files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-which git > /dev/null
+which git > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "Git isn't installed."
   exit
 fi
 
+# Clone dotfiles
 git clone https://github.com/nikdoof/dotfiles.git $HOME/.dotfiles > /dev/null
+
+# Clean bash files
+for file in .bash_profile .bashrc .bash_logout; do
+  if [ -e $file ]; then
+    rm -f $file
+  fi
+done
+
 cd $HOME/.dotfiles/
+
+# Stow the default packages
 for package in bin bash; do
   echo "Stowing ${package}"
   ./bin/bin/stowage $package
