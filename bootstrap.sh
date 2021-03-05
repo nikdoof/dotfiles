a9760fa8cdcc6ccd47bb40cc4f1a7d7ec6b8e7c7
Andrew Williams <andy@tensixtyone.com>
2021-03-05 10:46:34 +0000

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-git clone git@github.com:nikdoof/dotfiles.git $HOME/.dotfiles
+git clone https://github.com/nikdoof/dotfiles.git $HOME/.dotfiles > /dev/null
 cd $HOME/.dotfiles/
 for package in bin bash; do
   echo "Stowing ${package}"
   ./bin/bin/stowage $package
 done
+echo ""
+echo "Done, either source ~/.bash_profile or restart your shell."
