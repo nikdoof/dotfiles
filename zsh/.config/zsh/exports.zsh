298091b8d4b2a3db47841ac48b9d87c8a03fcdc1
Andrew Williams <andy@tensixtyone.com>
2021-03-24 18:12:18 +0000

    Fix zsh prompt colour

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,4 +4,4 @@ HISTSIZE=2000
 SAVEHIST=1000
 
 # Prompt
-export PS1="%F{black}[%F{white}%n@%m%F{black}] (%F{white}%1~%F{black}) %F{white}%# "
+export PS1="%F{black}[%F{white}%n@%m%F{black}] (%F{white}%1~%F{black}) %F{white}%#%F{39} "
