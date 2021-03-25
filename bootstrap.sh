54d5e591eefc9e2316da7e884d6a51c299bd4a4c
Andrew Williams <andy@tensixtyone.com>
2021-03-25 09:23:12 +0000

    Support clobbering existing files in stowage

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,17 +9,10 @@ fi
 # Clone dotfiles
 git clone https://github.com/nikdoof/dotfiles.git $HOME/.dotfiles > /dev/null
 
-# Clean bash files
-for file in .bash_profile .bashrc .bash_logout .zshrc; do
-  if [ -e $file ]; then
-    rm -f $file
-  fi
-done
-
 # Stow the default packages
 for package in bin shell-common bash zsh; do
   echo "Stowing ${package}"
-  $HOME/.dotfiles/bin/bin/stowage install $package
+  $HOME/.dotfiles/bin/bin/stowage --clobber install $package
 done
 echo ""
 echo "Done, either source ~/.bash_profile / ~/.zshrc or restart your shell."
