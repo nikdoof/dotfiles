6d429043b63b97503f3d26dc7ee618b2dbb7f6cd
Andrew Williams <andy@tensixtyone.com>
2021-04-11 13:45:30 +0100

    Update prompt

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,4 +4,4 @@ HISTSIZE=2000
 SAVEHIST=1000
 
 # Prompt
-export PS1="%F{black}[%F{white}%n@%m%F{black}] (%F{white}%1~%F{black}) %F{white}%#%F{39} "
+export PS1="%F{8}[%F{white}%n@%m%F{8}] (%F{white}%1~%F{8}) %F{white}%#%f "
