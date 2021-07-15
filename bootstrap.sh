5237913c4b0cc08d982d500cac9a78572135cee7
Andrew Williams <andy@tensixtyone.com>
2021-07-15 10:32:35 +0100

    [misc] Cleanup bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-which git > /dev/null 2>&1
+which git >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "Git isn't installed."
-  exit
+    echo "Git isn't installed."
+    exit
 fi
 
 # Clone dotfiles
-git clone https://github.com/nikdoof/dotfiles.git $HOME/.dotfiles > /dev/null
+git clone https://github.com/nikdoof/dotfiles.git $HOME/.dotfiles >/dev/null
 
 # Stow the default packages
 for package in bin shell-common bash zsh; do
-  echo "Stowing ${package}"
-  $HOME/.dotfiles/bin/bin/stowage --clobber install $package
+    echo "Stowing ${package}"
+    $HOME/.dotfiles/bin/bin/stowage --clobber install $package
 done
 echo ""
 echo "Done, either source ~/.bash_profile / ~/.zshrc or restart your shell."
