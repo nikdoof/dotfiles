5e0910798e7ce12e8052d6c5f85c323b65676a16
Andrew Williams <andy@tensixtyone.com>
2024-02-06 09:26:49 +0000

    Configure private packages as part of the bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,20 @@ if [ $? -ne 0 ]; then
 fi
 
 # Clone dotfiles
-git clone https://github.com/nikdoof/dotfiles.git $HOME/.dotfiles >/dev/null
+git clone git@github.com:nikdoof/dotfiles.git $HOME/.dotfiles >/dev/null
+git clone git@github.com:nikdoof/dotfiles-private.git $HOME/.dotfiles-private >/dev/null
 
-# Stow the default packages
+# Stow the default public packages
 for package in bin shell-common bash zsh; do
     echo "Stowing ${package}"
     $HOME/.dotfiles/bin/bin/stowage --clobber install $package
 done
+
+# Stow the default private packages
+for package in ssh; do
+    echo "Stowing ${package}"
+    $HOME/.dotfiles/bin/bin/stowage -r $HOME/.dotfiles-private --clobber install $package
+done
+
 echo ""
 echo "Done, either source ~/.bash_profile / ~/.zshrc or restart your shell."
