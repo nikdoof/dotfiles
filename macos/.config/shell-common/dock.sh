442043c7d99b4492761445c98dbabed4ac1453b2
Andrew Williams <andy@tensixtyone.com>
2025-03-19 23:36:02 +0000

    [macos] Update the dock from a config file

---
+++ macos/.config/shell-common/dock.sh
@@ -0,0 +1,15 @@
+# Updates the dock
+function update-dock() {
+    idx=1
+    while read entry; do
+        app_name=$(echo "$entry" | cut -d $'\t' -f 1)
+        app_path=$(echo "$entry" | cut -d $'\t' -f 2)
+        app_type=$(echo "$entry" | cut -d $'\t' -f 3)
+        idx=$((idx+1))
+        dockutil --no-restart -a "$(urldecode "$app_path")" > /dev/null 2>&1
+        if [ "$app_type" = "persisentApps" ]; then
+            dockutil --move "$app_name" -p $idx
+        fi
+    done < ~/.dotfiles/macos/.config/dockConfig.txt
+    killall Dock
+}
