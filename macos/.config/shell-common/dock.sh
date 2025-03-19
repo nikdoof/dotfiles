12b69729ec03ae085e56f57cb3ff64f93d95463b
Andrew Williams <andy@tensixtyone.com>
2025-03-19 23:41:06 +0000

    [macos] Use the app bundle ID

--- macos/.config/shell-common/dock.sh
+++ macos/.config/shell-common/dock.sh
@@ -5,10 +5,11 @@ function update-dock() {
         app_name=$(echo "$entry" | cut -d $'\t' -f 1)
         app_path=$(echo "$entry" | cut -d $'\t' -f 2)
         app_type=$(echo "$entry" | cut -d $'\t' -f 3)
+        bundle_id=$(echo "$entry" | cut -d $'\t' -f 5)
         idx=$((idx+1))
-        dockutil --no-restart -a "$app_path" > /dev/null 2>&1
+        dockutil --no-restart -a "$app_path" -l "$bundle_id" > /dev/null 2>&1
         if [ "$app_type" = "persisentApps" ]; then
-            dockutil --move "$app_name" -p $idx
+            dockutil --move "$app_name" -l "$bundle_id" -p $idx
         fi
     done < ~/.dotfiles/macos/.config/dockConfig.txt
     killall Dock
