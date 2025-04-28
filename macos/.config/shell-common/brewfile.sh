a2c4acda42a0fbf2494680c0d14811d49f7df312
Andrew Williams <andy@tensixtyone.com>
2025-04-28 20:00:59 +0100

    [shell-common] Quote vars

--- macos/.config/shell-common/brewfile.sh
+++ macos/.config/shell-common/brewfile.sh
@@ -1,4 +1,4 @@
 # Updates Homebrew installation from the Brewfile
 function update-brewfile() {
-    brew bundle check --file ~/.config/Brewfile || brew bundle --cleanup -f --file ~/.config/Brewfile
+    brew bundle check --file "$HOME/.config/Brewfile" || brew bundle --cleanup -f --file "$HOME/.config/Brewfile"
 }
