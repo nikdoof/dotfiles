2b3f64646ced613b3a612442f1795b5005c2220b
Andrew Williams <andrew.williams@portwest.com>
2025-07-30 13:40:43 +0100

    [macos] Combine macOS specific functions

--- macos/.config/shell-common/brewfile.sh
+++
@@ -1,4 +0,0 @@
-# Updates Homebrew installation from the Brewfile
-function update-brewfile() {
-    brew bundle check --file "$HOME/.config/Brewfile" || brew bundle --cleanup -f --file "$HOME/.config/Brewfile"
-}
--- macos/.config/shell-common/macos.sh
+++ macos/.config/shell-common/macos.sh
@@ -1,3 +1,17 @@
+# Tag the file as OK to run
+function itsok() {
+    if [[ $(uname) == "Darwin" ]]; then
+        xattr -d com.apple.quarantine $1
+    else
+        echo 'This only works on macOS...'
+    fi
+}
+
+# Updates Homebrew installation from the Brewfile
+function update-brewfile() {
+    brew bundle check --file "$HOME/.config/Brewfile" || brew bundle --cleanup -f --file "$HOME/.config/Brewfile"
+}
+
 # Updates the dock
 function update-dock() {
     idx=1
