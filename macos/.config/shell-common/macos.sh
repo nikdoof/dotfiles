78646a7e1d45aeb0ef8582a5bd9f5d2cfefcc43a
Andrew Williams <andy@tensixtyone.com>
2025-12-23 18:05:18 +0000

    [macos] Add XDG overrides

--- macos/.config/shell-common/macos.sh
+++ macos/.config/shell-common/macos.sh
@@ -34,3 +34,7 @@ function update-dock() {
     done < ~/.dotfiles/macos/.config/dotfiles/dockConfig.txt
     killall Dock
 }
+
+# Override XDG cache location for macOS to use the standard Library/Caches directory
+export XDG_CACHE_HOME="$HOME/Library/Caches"
+export XDG_RUNTIME_DIR="${TMPDIR}runtime-${UID}"
