e6e1c51a96f9710c61c3e32b2bd4969b7b287b34
Andrew Williams <andy@tensixtyone.com>
2025-12-23 18:22:12 +0000

    [zsh] Add completions

--- zsh/.config/zsh/completions.zsh
+++ zsh/.config/zsh/completions.zsh
@@ -1,19 +1,4 @@
+fpath+=($HOME/.config/zsh/completions)
+
 autoload -Uz compinit
 compinit
-
-function _add-sshkey() {
-    local -a identities
-
-    # check for .ssh folder presence
-    if [[ ! -d $HOME/.ssh ]]; then
-        return
-    fi
-    for id in $HOME/.ssh/id_ed25519_*; do
-        name=$(basename $id | cut -d'_' -f3- | cut -d'.' -f1)
-        [[ ${id:-3} != 'pub' ]] && identities+=$name
-    done
-
-    compadd $identities
-}
-compdef _add-sshkey add-sshkey
-
