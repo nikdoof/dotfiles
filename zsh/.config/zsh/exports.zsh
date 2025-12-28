b2da97d83e4f62f64ce65312f21d63c1de6eecb4
Andrew Williams <andy@tensixtyone.com>
2025-12-28 20:38:13 +0000

    [zsh] Update AWS prompt

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -5,5 +5,4 @@ SAVEHIST=1000
 
 # Prompt
 setopt PROMPT_SUBST
-export AWS_PROFILE_DISPLAY=''
-export PS1='%F{8}[%F{white}%n@%m%F{8}] (%F{white}%1~%F{8}) %F{yellow}$AWS_PROFILE_DISPLAY%F{white} %#%f '
+export PS1='%F{8}[%F{white}%n@%m%F{8}] (%F{white}%1~%F{8}) %F{yellow}$AWS_PROFILE%F{white} %#%f '
