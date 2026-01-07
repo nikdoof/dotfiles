2bffced05bd82a0238550671278bcbf03c522f09
Andrew Williams <andrew.williams@portwest.com>
2026-01-07 15:53:30 +0000

    [bash] Move history to XDG

--- bash/.config/bash/01_exports.bash
+++ bash/.config/bash/01_exports.bash
@@ -1,2 +1,6 @@
+# History
+mkdir -p "${XDG_STATE_HOME}"/bash
+export HISTFILE="${XDG_STATE_HOME}"/bash/history
+
 # Prompt
 export PS1="\[\e[0;90m\][\[\e[0;37m\]\u\[\e[0;37m\]@\[\e[0;37m\]\H\[\e[0;90m\]] \[\e[0;90m\](\[\e[0;37m\]\W\[\e[0;90m\]) \[\e[0;37m\]\$\[\e[0m\] "
