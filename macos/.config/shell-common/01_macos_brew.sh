176c2b9002203c65b4d3698e5da15bb54a9ae4e5
Andrew Williams <andrew.williams@portwest.com>
2026-01-08 10:06:00 +0000

    [macos] Make sure Homebrew installs Casks to ~/Applications

--- macos/.config/shell-common/01_macos_brew.sh
+++ macos/.config/shell-common/01_macos_brew.sh
@@ -1,5 +1,6 @@
 # Configure Homebrew environment
 if [ -x /opt/homebrew/bin/brew ]; then
     export HOMEBREW_NO_ENV_HINTS=1
+    export HOMEBREW_CASK_OPTS="--appdir=${HOME}/Applications"
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
