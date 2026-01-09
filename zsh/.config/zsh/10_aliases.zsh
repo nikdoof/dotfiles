1145c75b753a04a3663f57de4fcb187a29000b5c
Andrew Williams <andrew.williams@portwest.com>
2026-01-09 18:59:48 +0000

    [zsh] New aliases

---
+++ zsh/.config/zsh/10_aliases.zsh
@@ -0,0 +1,8 @@
+# Zsh specific aliases
+# Mostly suffix and global aliases
+
+# bat suffix aliases
+alias -s md='bat --style=header,grid --language=markdown'
+alias -s json='bat --style=header,grid --language=json'
+alias -s yml='bat --style=header,grid --language=yaml'
+alias -s yaml='bat --style=header,grid --language=yaml'
