511cb9fead84f20bdf3c25d73e7d3b43a5a9590f
Andrew Williams <andy@tensixtyone.com>
2026-01-10 09:09:56 +0000

    [zsh] Adjust history settings

--- zsh/.config/zsh/10_completions.zsh
+++ zsh/.config/zsh/10_completions.zsh
@@ -2,3 +2,8 @@ fpath+=($XDG_CONFIG_HOME/zsh/completions)
 
 autoload -Uz compinit
 compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
+
+# If we have fzf installed, load its zsh completions
+if [ -x $(command -v fzf) ]; then
+    source <(fzf --zsh)
+fi
