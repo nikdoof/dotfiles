a6a48cb0419891752a294bfcf433079e0f6fb3c7
Andrew Williams <andy@tensixtyone.com>
2026-01-12 00:33:10 +0000

    [starship] Add starship package

--- bash/.config/bash/01_exports.bash
+++ bash/.config/bash/01_exports.bash
@@ -3,4 +3,9 @@ mkdir -p "${XDG_STATE_HOME}"/bash
 export HISTFILE="${XDG_STATE_HOME}"/bash/history
 
 # Prompt
-export PS1="\[\e[0;90m\][\[\e[0;37m\]\u\[\e[0;37m\]@\[\e[0;37m\]\H\[\e[0;90m\]] \[\e[0;90m\](\[\e[0;37m\]\W\[\e[0;90m\]) \[\e[0;37m\]\$\[\e[0m\] "
+if [ -x $(command -v starship) ]; then
+    eval "$(starship init bash)"
+    export STARSHIP_CACHE="${XDG_CACHE_HOME}"/starship
+else
+    export PS1="\[\e[0;90m\][\[\e[0;37m\]\u\[\e[0;37m\]@\[\e[0;37m\]\H\[\e[0;90m\]] \[\e[0;90m\](\[\e[0;37m\]\W\[\e[0;90m\]) \[\e[0;37m\]\$\[\e[0m\] "
+fi
--- zsh/.config/zsh/01_exports.zsh
+++ zsh/.config/zsh/01_exports.zsh
@@ -6,5 +6,11 @@ SAVEHIST=$HISTSIZE
 setopt EXTENDED_HISTORY
 
 # Prompt
-setopt PROMPT_SUBST
-export PS1='%F{8}[%F{white}%n@%m%F{8}] (%F{white}%1~%F{8}) %F{yellow}$AWS_PROFILE%F{white} %#%f '
+# Use starship if installed, otherwise use a simple prompt
+if [ -x $(command -v starship) ]; then
+    eval "$(starship init zsh)"
+    export STARSHIP_CACHE="${XDG_CACHE_HOME}"/starship
+else
+    setopt PROMPT_SUBST
+    export PS1='%F{8}[%F{white}%n@%m%F{8}] (%F{white}%1~%F{8}) %F{yellow}$AWS_PROFILE%F{white} %#%f '
+fi
