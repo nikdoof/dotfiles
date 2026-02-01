f935b1f8c08b8ead0fd4b50bfd80c21369864d02
Andrew Williams <andy@tensixtyone.com>
2026-02-01 21:10:50 +0000

    [zed] Tidy up zed usage

--- zed/.config/shell-common/99_zed.sh
+++ zed/.config/shell-common/99_zed.sh
@@ -1,17 +1,2 @@
 # Zed specific shell configuration
-
-# Rather than installing or linking the Zed CLI, we define a function that
-# locates the Zed application and invokes its CLI directly.
-function zed() {
-    for zed_path in "$HOME/Applications/Zed.app" "/Applications/Zed.app"; do
-        if [ -d "$zed_path" ]; then
-            "$zed_path/Contents/MacOS/cli" "$@"
-            return
-        fi
-    done
-    echo "Zed application not found in common locations."
-    return 1
-}
-
-# Set Zed as the default visual, with wait flag
-export VISUAL="zed -w"
+export VISUAL="zed_wait"
