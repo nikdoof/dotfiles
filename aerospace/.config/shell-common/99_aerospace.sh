ab33d9f4a96a62c737812c094cc6c8967daab57e
Andrew Williams <andrew.williams@portwest.com>
2026-02-19 15:20:44 +0000

    [aerospace] ff -> ffw

--- aerospace/.config/shell-common/99_aerospace.sh
+++ aerospace/.config/shell-common/99_aerospace.sh
@@ -2,7 +2,7 @@
 
 # Fuzzy find and focus a window using aerospace and fzf
 if [ -x "$(command -v aerospace)" ] && [ -x "$(command -v fzf)" ]; then
-    function ff() {
+    function ffw() {
         aerospace list-windows --all | fzf --height 40% --layout=reverse --border --ansi | awk '{print $1}' | xargs -I {} aerospace focus --window-id {}
     }
 fi
