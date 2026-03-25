171a1c215ead61d72c0092347ff27b6dfdea5ec4
Andrew Williams <andy@tensixtyone.com>
2026-03-25 23:49:29 +0000

    [macos] Make sure the XDG_RUNTIME_DIR is correct no matter the input

--- macos/.config/shell-common/01_macos_xdg.sh
+++ macos/.config/shell-common/01_macos_xdg.sh
@@ -2,4 +2,4 @@
 
 # Override XDG cache location for macOS to use the standard Library/Caches directory
 export XDG_CACHE_HOME="$HOME/Library/Caches"
-export XDG_RUNTIME_DIR="${TMPDIR}runtime-${UID}"
+export XDG_RUNTIME_DIR="${TMPDIR%/}/runtime-${UID}"
